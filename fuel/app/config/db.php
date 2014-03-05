993e7e4316443a89a107ab2efb3388660c330f30
shiguma <b112030@cc.it-hiroshima.ac.jp>
2014-03-05 16:30:20 +0900

    config下の3ファイルを削除

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -3,8 +3,8 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_test_db',
-			'username'   => 'b112030',
+			'dsn'        => 'mysql:host=localhost;mysql:unix_socket=/var/lib/mysql/mysql.sock;dbname=fuel_test_db',
+			'username'   => 'root',
 			'password'   => 'vbserver'
 		),
 	),
